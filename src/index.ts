8cc24abd4ad6cf71a65688772dc9ff2976af42f4
PalmsDev <146481264+Yoxiba@users.noreply.github.com>
2023-12-11 21:53:44 +0000

    sum

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 console.log("Hello World!");
+fetch("pages/start.html")
+  .then((resp) => resp.text())
+  .then((html) => {
+    const rootElem = document.getElementById("root") as HTMLElement;
+    //if (rootElem) {
+    rootElem.innerHTML = html;
+    //}
+  });
