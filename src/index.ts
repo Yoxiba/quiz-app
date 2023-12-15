26a4a040de91677fa7360ff87277de5e757ea91a
PalmsDev <146481264+Yoxiba@users.noreply.github.com>
2023-12-15 20:07:38 +0000

    Capturar o nome do utilizador no input e armazenar no local storage

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,14 @@ fetch("pages/start.html")
     //}
   });
 //lol
+function salvarnome(nome: string): void {
+  localStorage.setItem("nome", nome);
+}
+function iniciarquiz(event: Event): void {
+  event.preventDefault();
+  const inputnome = document.getElementById("input-name") as HTMLInputElement;
+  if (inputnome) {
+    const nome = inputnome.value;
+    salvarnome(nome);
+  }
+}
